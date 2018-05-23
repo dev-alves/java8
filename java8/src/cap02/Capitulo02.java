fece84b98485e71cacf6ec7b8d2796c66776d284
dev-alves <victor.alvesf7@gmail.com>
2018-05-22 23:51:08 -0400

    Estudo do Capítulo 02

--- java8/src/cap02/Capitulo02.java
+++ java8/src/cap02/Capitulo02.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Capitulo02 {
-
+	
 	public static void main(String[] args) {
 		
 		Usuario user = new Usuario("vic1", 123, true);
@@ -15,9 +15,9 @@ public class Capitulo02 {
 		 * Arrays.asList() -> Maneira simles de criar uma list imutável
 		 */
 		List<Usuario> usuarios = Arrays.asList(user, user1, user2);
-		System.out.println("Imprimindo com o novo método forEach do pacote function do Java 8");
 		//Consumer<Usuario> mostrador = u -> System.out.println(u.toString());
 		
+		System.out.println("Imprimindo com o novo método forEach do pacote function do Java 8");
 		usuarios.forEach(u->System.out.println(u.toString()));
 	}
 
--- java8/src/cap02/Mostrador.java
+++ java8/src/cap02/Mostrador.java
@@ -2,6 +2,11 @@ package cap02;
 
 import java.util.function.Consumer;
 
+/**
+ * Interface funcional que contém apenas um método para implementação
+ * @author dev
+ *
+ */
 public class Mostrador implements Consumer<Usuario>{
 
 	@Override
