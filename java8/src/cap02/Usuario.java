6dbef829cc1d9df8a7b2af6b071931d279a31c60
Victor Alves <victor.alvesf7@gmail.com>
2018-08-01 20:26:01 -0400

    Novas funções e construtores

--- java8/src/cap02/Usuario.java
+++ java8/src/cap02/Usuario.java
@@ -5,6 +5,17 @@ public class Usuario {
 	private int pontos;
 	private boolean moderador;
 	
+	public Usuario() {}
+	
+	public Usuario(String nome) {
+		this.nome = nome;
+	}
+	
+	public Usuario(String nome, int pontos) {
+		this.nome = nome;
+		this.pontos = pontos;
+	}
+	
 	public Usuario(String nome, int pontos, boolean moderador) {
 		super();
 		this.nome = nome;
