908d1d83ec9a7b161f83f5e351085e4571d371db
Victor Alves <victor.alvesf7@gmail.com>
2018-08-23 20:15:54 -0400

    Atualizações

--- java8/src/cap07/IntStreamFamiliaStream.java
+++ java8/src/cap07/IntStreamFamiliaStream.java
@@ -1,7 +1,9 @@
 package cap07;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 import cap02.Usuario;
@@ -27,6 +29,27 @@ public class IntStreamFamiliaStream {
 				.average()
 				.getAsDouble();
 		System.out.println("Média: " + media);
+		
+		Double pontuacaoMedia = usuarios.stream()
+				.mapToDouble(Usuario::getPontos)
+				.average()
+				.orElse(0.0);
+		System.out.println("Pontuação Média: " + pontuacaoMedia);
+		
+		//lançando exceptions
+		Double pontuacaoException = usuarios.stream()
+				.mapToDouble(Usuario::getPontos)
+				.average()
+				.orElseThrow(IllegalStateException::new);
+		System.out.println("PontuaçãoException: " + pontuacaoException);
+		
+		//número de usuários com mais pontos
+		Optional<String> maioresPontuadores = usuarios.stream()
+				.max(Comparator.comparingInt(Usuario::getPontos))
+				.map(Usuario::getNome);
+		
+		if(maioresPontuadores.isPresent())
+				System.out.println(maioresPontuadores.get());
 	}
 
 }
--- java8/src/cap07/Streams.java
+++ java8/src/cap07/Streams.java
@@ -23,12 +23,13 @@ public class Streams {
 		usuarios.forEach(System.out::println);
 		
 		//Tornando em moderadores os três usuários com mais pontos
+		System.out.println("\nTornando moderador");
 		usuarios.sort(Comparator.comparing(Usuario::getPontos).reversed());
 		usuarios.subList(0, 3).forEach(Usuario::tornarModerador);
 		usuarios.forEach(System.out::println);
 		
 		//filtrando usuários com mais de 12 pontos
-		System.out.println("filtrando usuários com mais de 100 pontos");
+		System.out.println("\nfiltrando usuários com mais de 100 pontos");
 		Stream<Usuario> streamUsuario = usuarios.stream().filter(u-> u.getPontos() > 12);//um stream devolve um stream, exemplo
 		streamUsuario.forEach(System.out::println);
 		
@@ -40,6 +41,9 @@ public class Streams {
 		usuarios.forEach(System.out::println);
 		
 		//Obs: O Stream não tem efeito colateral sobre a coleção que o originou
+		
+		System.out.println("\nVerificando se há efeito colateral na lista de objetos");
+		usuarios.forEach(System.out::println);
 	}
 
 }
